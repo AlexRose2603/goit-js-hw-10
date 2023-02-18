d930f2ea68e31dd6bd6d6fea60ec2596e4384f08
AlexRose2603 <111235206+AlexRose2603@users.noreply.github.com>
2023-02-18 12:12:57 +0000

    Deploying to gh-pages from @ AlexRose2603/goit-js-hw-10@4a348585e8bf30ed746640d8486fbe226bbec3da 🚀

--- index.2840f8c7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(e){if(console.log(e.currentTarget.value),!e.currentTarget.value.trim())return}))}();
-//# sourceMappingURL=index.2840f8c7.js.map
